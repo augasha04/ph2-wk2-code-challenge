8d729cc5200ac5dd9c363114524f7088b2b425e7
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-30 14:22:54 +0300

    finished with fetching data, know is imlementing the methods taugt

---
+++ transaction/src/components/BotsCollection.js
@@ -0,0 +1,19 @@
+// importing useEffect
+import React, { useEffect, useState } from "react";
+
+// ...importing the rest of the data
+
+function BotsCollection() {
+        const [botSelected, setBotSelected] = useState("All");
+        const [items, setItems] = useState([]);
+      
+        // Add useEffect hook
+
+        useEffect(() => {
+          fetch("http://localhost:4000")
+            .then((r) => r.json())
+            .then((items) => setItems(items));
+        }, []);
+}
+
+export default BotsCollection
---
+++ transaction/src/components/YourBotArmy.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+function MyForm(){
+
+    const[data,setData]=useState({
+        date:"",
+        description:"",
+        category :"",
+        amount:"",
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const NewData={...data}
+        NewData[e.target.id]=e.target.value
+        setData(NewData)
+        console.log (NewData)
+      }
+      fetch("", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((r) => r.json())
+    .then((data) => console.log(data));
+
+
+
+  return (
+    <div className='transform'>
+        <form>
+        <h2>INPUT FORM</h2>
+
+        {/* <input type = "ID" placeholder ="ID"/> */}
+
+<input type = "DATE" placeholder = "DATE"/>
+<input type = "DESCRIPTION" placeholder ="DESCRIPTION"/>
+<input type = "CATEGORY" placeholder ="CATEGORY"/>
+<input type = "AMOUNT" placeholder ="AMOUNT"/>
+<button type='button'onClick={handleSubmit}>submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default MyForm;
